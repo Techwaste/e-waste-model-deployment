b5aba8dc6e7939dff30ad5b1adc5109ae433d8b2
danielprasetyo7952 <vyverdrake007@gmail.com>
2023-05-04 21:19:20 +0700

    Update Preprocessing Image

--- main.py
+++ main.py
@@ -16,30 +16,10 @@ class_names = ['battery', 'cable', 'crt_tv', 'e_kettle', 'fridge', 'keyboard',
 def preprocess_image(image):
     image = image.resize((300, 300))
     image_array = np.array(image)
-    image_array = tf.keras.applications.resnet50.preprocess_input(image_array)
+    image_array = tf.keras.applications.xception.preprocess_input(image_array)
     image_array = np.expand_dims(image_array, axis=0)
-    # Load the image
-    # image = tf.io.read_file(image)
-    # try:
-    #     image = tfi.decode_jpeg(image, channels=3)
-    # except:
-    #     image = tfi.decode_png(image, channels=3)
-    
-    # # Change the image data type
-    # image = tfi.convert_image_dtype(image, tf.float32)
-    
-    # # Resize the Image
-    # image = tfi.resize(image, (300, 300))
-    
-    # # Convert image data type to tf.float32
-    # convert_img = tf.cast(image, tf.float32)
-    
-    # resized_image = np.reshape(convert_img, (300, 300, 3))
-    
-    # tf_image = resized_image[tf.newaxis, ...]
     
     return image_array
-    # return tf_image
 
 def predict_image(image):
     predictions = model.predict(image)
@@ -57,10 +37,8 @@ def read_root():
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
     contents = await file.read()
-    # image = tf.io.read_file(file)
     image = Image.open(io.BytesIO(contents))
     tf_image = preprocess_image(image)
-    # return image
     predict_class, predict_probability = predict_image(tf_image)
     
     return f"{predict_class} : {predict_probability}"
