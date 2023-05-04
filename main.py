14206ae72ad1a5c689518052d4ec7dfb43377aaa
Daniel Budi Prasetyo <101175980+danielprasetyo7952@users.noreply.github.com>
2023-05-04 21:48:34 +0700

    API for E-Waste Model Xception (#1)
    * Add main.py
    
    * Update Preprocessing Image
    
    * remove unused import and exception input
    
    - keras and image import
    - input must ("jpg", "jpeg", "png")
    
    ---------
    
    Co-authored-by: Caesario Dito <caesariodito2014@gmail.com>

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, File, UploadFile
+
+import io
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+
+model = tf.keras.models.load_model('model/xception_latest.h5')
+app = FastAPI()
+class_names = ['battery', 'cable', 'crt_tv', 'e_kettle', 'fridge', 'keyboard',
+               'laptop', 'light_bulb', 'monitor', 'mouse', 'pcb',
+               'phone', 'printer', 'rice_cooker', 'washing_machine']
+
+def preprocess_image(image):
+    image = image.resize((300, 300))
+    image_array = np.array(image)
+    image_array = tf.keras.applications.xception.preprocess_input(image_array)
+    image_array = np.expand_dims(image_array, axis=0)
+    
+    return image_array
+
+def predict_image(image):
+    predictions = model.predict(image)
+    prediction_idx = np.argmax(predictions)
+    
+    predicted_class_name = class_names[prediction_idx]
+    predicted_probability = np.max(predictions) * 100
+    
+    return predicted_class_name, predicted_probability
+
+@app.get("/")
+def read_root():
+    return {"Welcome to TechWas (Technology Waste), Daniel"}
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    if not extension:
+        return "Image must be jpg, jpeg, or png format!"
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))
+    tf_image = preprocess_image(image)
+    predict_class, predict_probability = predict_image(tf_image)
+    
+    return f"{predict_class} : {predict_probability}"
+    
