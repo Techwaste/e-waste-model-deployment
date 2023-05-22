3adc1ec24e8bbdad91654e4000cfc92e08dc3962
danielprasetyo7952 <vyverdrake007@gmail.com>
2023-05-22 21:52:50 +0700

    Update Function

--- func.py
+++ func.py
@@ -2,9 +2,6 @@ import numpy as np
 import tensorflow as tf
 
 model = tf.keras.models.load_model('model/xception_latest.h5')
-# class_names = ['battery', 'cable', 'crt_tv', 'e_kettle', 'fridge', 'keyboard',
-#                'laptop', 'light_bulb', 'monitor', 'mouse', 'pcb',
-#                'phone', 'printer', 'rice_cooker', 'washing_machine']
 class_names = ['Baterai', 'Kabel', 'CRT TV', 'E-kettle', 'Kulkas',
                'Keyboard', 'Laptop', 'Bolam', 'Monitor', 'Mouse',
                'PCB', 'Phone', 'Printer', 'Rice Cooker', 'Mesin Cuci']
@@ -25,17 +22,3 @@ def predict_image(image):
     predicted_probability = np.max(predictions) * 100
     
     return predicted_class_name, predicted_probability
-
-# def convert_png_to_jpg(image):
-#     png_image = tf.io.read_file(image)
-#     png_image = tf.image.decode_png(png_image)
-#     png_image = tf.cast(png_image, tf.uint8)
-#     png_image = tf.image.encode_jpeg(png_image)
-    
-#     # image = image.convert('RGB')
-#     # jpeg_data = io.BytesIO()
-#     # image.save(jpeg_data, format='JPEG')
-    
-#     # return jpeg_data.getvalue()
-    
-#     return png_image
